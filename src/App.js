7476f65e1a3ab344a3297e53b6a2fe029ddd741f
nguyenling <nguyenlinh982001@gmail.com>
2022-06-30 09:16:38 +0700

    catch error 404

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages
 import WarpPages from "./pages/components/WarpPages";
-import NavBar from "./pages/components/NavBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Detail from "./pages/Detail";
 import Category from "./pages/Category";
 // admin
 import Manager from "./pages/Manager";
+// ErrorBoundary
+import ErrorBoundary from "./pages/components/ErrorBoundary";
+import Error404 from "./pages/components/Error404";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<WarpPages page={<Home />} type={0} />} />
-          <Route
-            path="/post/:id"
-            element={<WarpPages page={<Detail />} type={0} />}
-          />
-          <Route
-            path="/category/:category"
-            element={<WarpPages page={<Category />} type={0} />}
-          />
-          <Route path="/admin/login" element={<Login />} />
-          <Route path="/admin/blogs" element={<Manager />} />
-        </Routes>
-      </BrowserRouter>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<WarpPages page={<Home />} type={0} />} />
+            <Route
+              path="/post/:id"
+              element={<WarpPages page={<Detail />} type={0} />}
+            />
+            <Route
+              path="/category/:category"
+              element={<WarpPages page={<Category />} type={0} />}
+            />
+            <Route path="/admin/login" element={<Login />} />
+            <Route path="/admin/blogs" element={<Manager />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
     </>
   );
 }
---
+++ src/pages/components/Error404.jsx
@@ -0,0 +1,10 @@
+const Error404 = () => {
+  return (
+    <>
+      <div class="d-flex align-items-center justify-content-center vh-100 bg-primary">
+        <h1 class="display-1 fw-bold text-white">404</h1>
+      </div>
+    </>
+  );
+};
+export default Error404
