ff8a50f9908b25901b9729f04601928ff7fe2413
nguyenling <nguyenlinh982001@gmail.com>
2022-07-07 18:51:07 +0700

    category when no data

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -19,21 +19,28 @@ function Index() {
   return (
     <>
       <div className="row">
-        {posts.map((post) => {
-          return (
-            <div className="col-md-4">
-              <PostCard
-                id={post["_id"]}
-                url={post["url"]}
-                title={post["title"]}
-                description={post["description"]}
-                content={post["content"]}
-                created={post["createdAt"]}
-                views={post['views']}
-              />
-            </div>
-          );
-        })}
+        {posts.length > 0 ? (
+          posts.map((post) => {
+            return (
+              <div className="col-md-4">
+                <PostCard
+                  id={post["_id"]}
+                  url={post["url"]}
+                  title={post["title"]}
+                  description={post["description"]}
+                  content={post["content"]}
+                  created={post["createdAt"]}
+                  views={post["views"]}
+                />
+              </div>
+            );
+          })
+        ) : (
+          <div className="col">
+            <h1 className="text-center">no data yet</h1>
+          </div>
+        )}
+        {}
       </div>
     </>
   );
